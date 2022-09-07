9f02c1e3e9414450fad06cd7b0694ed9c101d8ab
xxhernandoxx <hernandezm2319@gmail.com>
2022-09-06 20:40:08 -0500

    Create a grid display for the label widget

--- interface_boilerplate.py
+++ interface_boilerplate.py
@@ -3,10 +3,14 @@ from tkinter import *
 root = Tk()
 
 # creating a label widget
-myLabel = Label(root, text="Hello World!")
+myLabel_1 = Label(root, text="Hello World!")
+myLabel_2 = Label(root, text="My name is Matthew Hernandez")
 
 # displaying on the screen
-myLabel.pack()
+myLabel_1.grid(row=0, column=0)
+myLabel_2.grid(row=1, column=0)
 
+
+# running loop for window
 root.mainloop()
 
